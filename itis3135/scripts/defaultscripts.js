16bb0aa7083413f61910664c0b45544c20ff1c8e
Emily <89706868+egustaf2@users.noreply.github.com>
2022-10-12 13:04:20 -0400

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -31,10 +31,10 @@ function dateAndTime()
     document.getElementById("dateTimeOutput").innerHTML = fullDateAndTime;
 }
 
-function welcome()
+function greeting()
 {
     let name = document.getElementById("name").value;
     let feeling = document.getElementById("feeling").value;
-    let fullWelcome = "Gustafson Designs welcomes you, " + name + "! We're glad that you are " + feeling + "!";
-    document.getElementById(welcomeOutput).innerHTML = fullWelcome;
+    let welcome = "Gustafson Designs welcomes you, " + name + "! We're glad that you are " + feeling + "!";
+    document.getElementById(welcomeOutput).innerHTML = welcome;
  }
