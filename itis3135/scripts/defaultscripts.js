b9a708b1133ffda15d92059b42b23b0d45f10c79
Emily <89706868+egustaf2@users.noreply.github.com>
2022-10-12 13:25:56 -0400

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -36,5 +36,5 @@ function welcome()
     let name = document.getElementById("name").value;
     let feeling = document.getElementById("feeling").value;
     let fullWelcome = "Gustafson Designs welcomes you, " + name + "! We're glad that you are " + feeling + "!";
-    document.getElementById(welcomeOutput).innerHTML = welcome;
+    document.getElementById("welcomeOutput").innerHTML = fullWelcome;
  }
