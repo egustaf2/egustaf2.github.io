8f8ef24d292e2639479c9afaf429163b75dbc230
Emily <89706868+egustaf2@users.noreply.github.com>
2022-10-17 17:36:33 -0400

    Update calculator_try.js

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,13 @@
+class Calculator
+{
+    constructor(calculator, keys, display)
+    {
+        this.calculator = calculator;
+        this.display = display;
+    }
+
+    turnOn()
+    {
+        this.keys.addEventListen
+    }
+}
